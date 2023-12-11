3c134d5ffa00c03a41ea1b31949c75afb365397a
musa <musaqazim1@gmail.com>
2023-12-12 00:34:07 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,113 @@
+// Question no 1;
+// var name = prompt('Enter your name').toUpperCase()
+// var nameLast = prompt('Enter your last name').toUpperCase() ;
+// var fullName ="Assalam o Alikum " +  name + nameLast 
+
+// document.write('<h1>'+fullName+'</h1>')
+
+
+
+// Question no 2;
+
+// var user = prompt( 'Enter your favourite Mobile phone Name');
+
+// var userLength = user.length
+
+
+// document.write('<h1>'+'My favourite phone is : '+user+'<h1/>')
+// document.write('<h1>'+'Your word length is : '+userLength+'<h1/>')
+
+
+
+// Question no 3;
+
+// var country = 'pakistani';
+  
+// var find = country.indexOf('n');
+
+// document.write('<h1>','string : ',country,'<h1/>')
+// document.write('<h1>'," Index of 'n' : ",  find ,'<h1/>')
+
+
+// Question no 4;
+
+// var country = 'Hellow World';
+  
+// var find = country.lastIndexOf('l');
+
+// document.write('<h1>','string : ',country,'<h1/>')
+// document.write('<h1>'," lastIndex of 'l' : ",  find ,'<h1/>')
+
+
+// Question no 5;
+
+// var country = 'pakistani';
+  
+// var find = country[3];
+
+// document.write('<h1>','string : ',country,'<h1/>')
+// document.write('<h1>',"Character at Index '3' : ",  find ,'<h1/>') 
+
+// Question no 6;
+
+// var city = 'Hyderabad';
+// var cityNew = city.replace(city,'Islamabad')
+
+// document.write('<h1>','City : ',city,'<h1/>','<br>')
+// document.write('After replacement : ',cityNew)
+
+
+// Question no 7;
+
+
+// var message = "Ali and Sami are best friends.They play circket and football together"
+// var newMessage = message.replaceAll('and','&')
+
+// document.write('<h1>',message,'</h1>')
+// document.write('<h1>',newMessage,'</h1>')
+
+
+// Question no 8;
+
+// var num = 472;
+// var numNew = num.toString()
+
+
+// document.write('<h1>','Value : ',numNew,'<h1/>')
+// document.write('Type string');
+// document.write('<h1>','Value : ',num,'<h1/>')
+// document.write('Type Number')
+
+
+// Question no 8;
+
+// var input = 'peanuts';
+// var inputUpper = input.toUpperCase()
+
+
+// document.write('<h1>','Use input : ',input,'<h1/>')
+// document.write('<h1>','Use Upper Case : ',inputUpper,'<h1/>')
+
+// Question no 9;
+  
+// var input = 'javacsript';
+ 
+// var update = input[0].toUpperCase()+input.slice(1)
+
+// document.write('<h1>','User Input : ',input,'<h1/>')
+// document.write('<h1>','Title Case : ',update,'<h1/>')
+
+// Question no 10;
+
+
+var num = 35.36;
+var numNew = num.toString();
+var numNews = numNew.replace('.','')
+
+
+document.write('<h1>','Number : ',num,'<h1/>')
+
+document.write('<h1>','Result : ',numNews,'<h1/>')
+
+
+
